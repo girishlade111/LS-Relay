5cb0dd7bc8ad13f6ba7a8301072577cd87a46bd6
Girish Lade <girishlade111@gmail.com>
2026-08-22 06:05:09 -0700

    feat: add Clerk sign-up page route to auth directory

---
+++ ls-ship/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "@clerk/nextjs";
+
+export const metadata = {
+  title: "Sign up - LS-Ship",
+};
+
+export default function SignUpPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-bg">
+      <SignUp />
+    </main>
+  );
+}
